802f0b18443e61c56e3804cf74ebeb2675baaaca
camilla <camilla.gausland@gmail.com>
2020-06-05 12:05:00 +0200

    seeding

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,9 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import crypto from 'crypto'
 import bcrypt from 'bcrypt-nodejs'
+import productData from '../frontend/src/productData.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/webShop"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/finishProject"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
@@ -27,6 +28,35 @@ const User = mongoose.model('User', {
     default: () => crypto.randomBytes(128).toString('hex')
   }
 })
+
+const Product = mongoose.model('Product', {
+  name: String,
+  price: Number, 
+  currency: String,
+  height: Number, 
+  width: Number,
+  mesurement: String,
+  img: String,
+  descreption: String
+})
+
+ if (process.env.RESET_DATABASE) {
+  console.log('resetting the database')
+  const seedDatabase = async () => {
+    await Product.deleteMany()
+
+    const blue = new Product({name: 'Blue', price: 40, currency: '€', height: 30, width: 30, mesurement:'cm'})
+    await blue.save()
+
+    productData.forEach((product) => {
+      new Product(product).save()
+    })
+    
+  }
+  seedDatabase()
+}
+
+
 
 const authenticateUser = async (req, res, next) => {
   try {
@@ -58,6 +88,11 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/products', async (req, res) => {
+  const products = await Product.find()
+  res.json(products)
 })
 
 app.post('/users', async (req, res) => {
